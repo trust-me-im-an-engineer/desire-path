14d3923ff1ed4d5ee5d610382f99100c8035faec
trust-me-im-an-engineer <isavelij4@gmail.com>
2026-09-09 10:11:48 +0000

    interest points data texture

--- src/interest-points/interest-points.ts
+++ src/interest-points/interest-points.ts
@@ -2,8 +2,11 @@ import * as THREE from "three";
 
 import { InterestPoint } from "./interest-point";
 
+export const TEXTURE_WIDTH = 8;
+
 export class InterestPoints {
 	public readonly group: THREE.Group;
+	public readonly texture: THREE.Texture;
 
 	constructor(
 		public points: InterestPoint[],
@@ -17,5 +20,24 @@ export class InterestPoints {
 			pointMesh.position.set(point.nativePosition.x, -point.nativePosition.y, 4);
 			this.group.add(pointMesh);
 		}
+
+		const data = new Float32Array(3 * TEXTURE_WIDTH ** 2);
+		for (let i = 0; i < data.length; i += 3) {
+			data[i] = points[0].nativePosition.x;
+			data[i + 1] = points[0].nativePosition.y;
+			data[i + 2] = points[0].weight;
+		}
+		this.texture = new THREE.DataTexture(
+			data,
+			TEXTURE_WIDTH,
+			TEXTURE_WIDTH,
+			THREE.RGBFormat,
+			THREE.FloatType,
+		)
+		this.texture.minFilter = THREE.NearestFilter;
+		this.texture.magFilter = THREE.NearestFilter;
+		this.texture.generateMipmaps = false;
+		this.texture.colorSpace = THREE.NoColorSpace;
+		this.texture.needsUpdate = true;
 	}
 }
